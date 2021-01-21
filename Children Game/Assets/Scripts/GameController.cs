cd761ca2341f11c859ed721a7193b7b80e262f0a
KendrickwithCode <76670582+KendrickwithCode@users.noreply.github.com>
2021-01-21 22:35:24 +1000

    First Commit
    Added Unity Project to GitHub

---
+++ Children Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public InputField input;
+    public Vector3 moveDirection = Vector3.zero;
+    public GameObject Player;
+
+    void Awake()
+    {
+
+        input = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    public void GetInput(string PlayerText)
+    {
+
+        Debug.Log("You entered " + PlayerText);
+        
+        //Controls for left, right, up and down.
+        if (input.text == "left")
+        {
+            Player.transform.Translate(Vector3.left * 20.0f);
+            input.text = "";
+        }
+        if (input.text == "right")
+        {
+            Player.transform.Translate(Vector3.right * 20.0f);
+            input.text = "";
+        }
+        if (input.text == "up")
+        {
+            Player.transform.Translate(Vector3.forward * 20.0f);
+            input.text = "";
+        }
+        if (input.text == "down")
+        {
+            Player.transform.Translate(Vector3.back * 20.0f);
+            input.text = "";
+        }
+
+    }
+}
---
+++ Children Game/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    private static string Text;
+    public Rigidbody rb;
+    public float speed = 18;
+
+    void Update()
+    {
+        //Storing text from GameController.cs
+        //Text = GameController.PlayerText;
+      //  if (Text == "left")
+      //  {
+       //     Debug.Log("HOLY SHIT!");
+      //  }
+    }
+}
